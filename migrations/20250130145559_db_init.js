9d2f899497d28563e2dbff53c6ab61d563ca91fe
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-02-01 15:19:07 +0100

    fix: use tests

--- migrations/20250130145559_db_init.js
+++ migrations/20250130145559_db_init.js
@@ -2,19 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable('users', table => {
-        table.increments('id');
-        table.string('first_name');
-        table.string('last_name');
-        table.date('birth_date');
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.text('first_name');
+    table.text('last_name');
+    table.date('birth_date');
+  })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('users');
 };
--- plugins/db.js
+++ plugins/db.js
@@ -6,6 +6,6 @@ const { PrismaClient } = require('@prisma/client')
 const { dbConfig } = require('../knexfile')
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.decorate('knex', knex(dbConfig))
   fastify.decorate('prisma', new PrismaClient())
+  fastify.decorate('knex', knex(dbConfig))
 })
